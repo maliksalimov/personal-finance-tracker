00722600db031df03df8d781fa7b2a7cd023f96a
skull-crusher-aze <malik.salimov.2006@gmail.com>
2025-07-29 13:00:26 +0400

    category dto is done

--- src/main/java/com/maliksalimov/financetrackerapi/dto/category/CategoryDto.java
+++ src/main/java/com/maliksalimov/financetrackerapi/dto/category/CategoryDto.java
@@ -1,4 +1,25 @@
 package com.maliksalimov.financetrackerapi.dto.category;
 
+import com.maliksalimov.financetrackerapi.entity.enums.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String color;
+    private String icon;
+    private TransactionType type;
+    private Boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
